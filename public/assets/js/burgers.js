5bdf78ddc819051f0047c95e422e82be9e4ce952
Alvy <floressuarezalvaro@gmail.com>
2021-01-12 20:21:31 -0800

    updating burgers to devoured

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -2,4 +2,28 @@ document.addEventListener('DOMContentLoaded', (event) => {
     if (event) {
         console.log("DOM loaded")
     }
+    const dButton = document.querySelectorAll(".devourButton");
+    if(dButton) {
+        dButton.forEach((event) => {
+            event.addEventListener('click', (e) => {
+                e.preventDefault();
+                console.log(`E.target is ${e.target}`)
+                const id = e.target.getAttribute('data-id');
+                const devoured = {
+                    id: id,
+                };
+                fetch(`/api/burgers/${id}`, {
+                    method: "PUT",
+                    headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(devoured),
+                }).then(() => {
+                    location.reload("/");
+                });
+            })
+        })
+    }
+
 })
